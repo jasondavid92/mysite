a9ef66113405acea0267e70c84c28aff8cda45a8
jasondavid92 <jason.david@usma.edu>
2015-02-25 11:11:05 -0500

    HW4 Fix

--- polls/views.py
+++ polls/views.py
@@ -18,11 +18,6 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-	def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -30,6 +25,10 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+	
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
     try:
